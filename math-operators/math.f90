e8aaea435f4ceffab76d2c155051bfddb1a5ee10
Robert Carr <robertcarrcisd@gmail.com>
2022-09-04 23:19:24 -0500

    Added math operations directory

---
+++ math-operators/math.f90
@@ -0,0 +1,8 @@
+program math
+    ! This file will be heavily commented because I want to learn and remember
+    ! what I was doing long from when I complete this
+    implicit none
+    
+    
+
+end program math
